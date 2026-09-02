14bc3a338dc53de6112ac09b5bdd3e1db9b548a6
cloudwithpreetham <preetham.pereira@icloud.com>
2026-09-02 09:33:00 +0530

    style(region): align aws_autoscaling_group attributes (terraform fmt)

--- terraform/modules/region/asg.tf
+++ terraform/modules/region/asg.tf
@@ -43,10 +43,10 @@ resource "aws_launch_template" "app" {
 }
 
 resource "aws_autoscaling_group" "app" {
-  name                = "${var.project_name}-${local.role}-app-asg"
-  vpc_zone_identifier = aws_subnet.private[*].id
-  target_group_arns   = [aws_lb_target_group.app.arn]
-  health_check_type   = "ELB"
+  name                      = "${var.project_name}-${local.role}-app-asg"
+  vpc_zone_identifier       = aws_subnet.private[*].id
+  target_group_arns         = [aws_lb_target_group.app.arn]
+  health_check_type         = "ELB"
   health_check_grace_period = 60
 
   min_size         = var.asg_min_size
